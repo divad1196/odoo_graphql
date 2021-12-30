f16eeba7d72faed6f8b7558e3886a033edcf0bf0
David Gallay <davidtennis96@hotmail.com>
2021-12-30 16:10:34 +0100

    add permission module

---
+++ odoo_graphql_permissions/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# from . import http  # attempt to add a custom handler
+from . import controllers
+from .utils import parse_document
---
+++ odoo_graphql_permissions/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import graphql_handler
