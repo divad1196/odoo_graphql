590cad3437b2471d314a182c29b59abc522587c9
Gallay David <david.gallay.96@gmail.com>
2024-07-02 22:00:23 +0200

    Handle case where Date/Datetime fields are not defined

--- odoo_graphql/graphql_resolver.py
+++ odoo_graphql/graphql_resolver.py
@@ -374,6 +374,8 @@ def _get_type_serializer_date(field, variables=None):
     data = args2dict(field.arguments)
     fmt = data.get("format")
     def func(value):
+        if value is False:
+            return False
         if fmt:
             return value.strftime(fmt)
         return value.toordinal()
@@ -386,6 +388,8 @@ def _get_type_serializer_datetime(field, variables=None):
         tz = pytz.timezone(tz)
     fmt = data.get("format")
     def func(value):
+        if value is False:
+            return False
         if tz:
             value = value.replace(tzinfo=pytz.utc).astimezone(tz)
         if fmt:
