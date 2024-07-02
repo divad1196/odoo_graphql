d7395426fb5c320c1830414e182136bd1be129e4
Gallay David <david.gallay.96@gmail.com>
2024-07-02 23:59:52 +0200

    Making the tests work

--- odoo_graphql_test/tests/utils.py
+++ odoo_graphql_test/tests/utils.py
@@ -11,6 +11,11 @@ def open_query(file, *args, **kw):
     file = (_current_file_path() / file).resolve()
     return open(file, *args, **kw)
 
+def get_query(file, *args, **kw):
+    with open_query(file, *args, **kw) as f:
+        query = f.read()
+    return query
+
 def contains(data, fields):
     return all(f in data for f in fields)
 
